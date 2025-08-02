20ffad3d890c44946451e682eacf1cf43d7a43d4
harshpreet284 <harshpreetsingh2643@gmail.com>
2025-08-02 14:04:41 +0530

    add

---
+++ Javascript/Lect 40/index.js
@@ -0,0 +1,44 @@
+// var age = 25;
+
+// if (true){
+//     console.log(age);
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+// solve();
+
+// var x = 10;
+// var x = 20;
+
+// {
+//     let a = 10;
+//     console.log (a);
+// }
+
+// let a = 19;
+// a = 20;
+// console.log (a );
+
+// let a = 10;
+// a= "Harsh";
+// a= true;
+// a= null;
+// console.log(a);
+
+// const a= 20;
+// console.log(a);
+// const a = 29;
+// console.log(a);
+
+// let marks = 20;
+// marks= 20.3532;
+// marks= "Harsh";
+// marks= true;
+
+// let marks = 1243902589494303893048204822634682346285283282685282927346285293482592372852975729389572925027395275930572502;
+let mark;
+console.log (typeof(mark));
