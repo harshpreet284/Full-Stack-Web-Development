4da090ceabda40a6f21ea7b4fc7a5b7be314186a
harshpreet284 <harshpreetsingh2643@gmail.com>
2025-08-03 14:31:02 +0530

    add

---
+++ Javascript/Lect 41/index.js
@@ -0,0 +1,48 @@
+// Operators
+
+// let a = 2;
+// let b = 5;
+
+// a= a+ 3;
+// a += 3;
+// console.log(a);
+// console.log(a**b);
+
+// console.log(10<5);
+
+// Loose equality and Strict Equality
+
+// console.log ('2'!= 2);
+// console.log ('2' !== 2);
+
+// let age = 25;
+// let status1 = (age>18) ? 'I can vote' : 'I cannot vote';
+// console.log(status1);
+
+// let ans = !(false);
+// console.log (ans);
+
+// let ans = ~(0);
+// console.log (ans);
+
+// let age = 20;
+// if(age > 18)
+// {
+//     console.log("Can Vote");
+// }
+// else
+// {
+//     console.log("Cannot Vote");
+// }
+
+let a = 3;
+switch(a)
+{
+    case 1: console.log('A');
+    break;
+    case 2: console.log('B');
+    break;
+    case 3: console.log('C');
+    break;
+    default: console.log('D');
+}
