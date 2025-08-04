8a5a53c137fd2f8bc736c8f7b626aefe3304cf33
harshpreet284 <harshpreetsingh2643@gmail.com>
2025-08-04 13:11:08 +0530

    add

---
+++ Javascript/Lect 43/index.js
@@ -0,0 +1,40 @@
+// function myName(){
+//     console.log("Harshpreet Singh");
+// }
+
+// myName();
+
+// function getSum(a,b,c){
+//     let sum = a+b+c;
+//     return sum;
+// }
+
+// let ans= getSum(1,2,3);
+// console.log("Sum ="+ ans);
+
+// function newMultiply(a,b){
+//     let mul=a*b;
+//     return mul;
+// }
+// let ans= newMultiply(2,3);
+// console.log("Product ="+ ans);
+
+// Function Expression
+
+// let newMultiply = function (a,b){
+//     let mul= a*b;
+//     return mul;
+// }
+// let ans= newMultiply(2,3);
+// console.log("Product ="+ ans);
+
+// Arrow Functions
+
+let newMultiply = (a,b) =>{
+    let mul = a*b;
+    return mul;
+}
+let ans = newMultiply(2,3);
+console.log("Product ="+ans);
+
+
