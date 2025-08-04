451203b3f5e52d9796b1a17c633305cb97318a52
harshpreet284 <harshpreetsingh2643@gmail.com>
2025-08-04 14:05:42 +0530

    add

---
+++ Javascript/Lect 44/index.js
@@ -0,0 +1,22 @@
+// Objects
+
+// let obj = {
+//     name: "Harsh",
+//     age: 22,
+//     weight: 73,
+//     "full name": "Harshpreet Singh",
+//     greet: function(){
+//         console.log("Hello");
+//     }
+// }
+// console.log(obj);
+// obj.greet();
+
+
+// let arr=[1,2,3,4,5];
+// console.log(arr);
+
+// Array Constructors
+
+// let arr= new Array("Hello", 2, "b2", 3);
+// console.log(arr);
