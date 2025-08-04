de9b289fb0e825eb7357de2d9cc9e4845d53fbf2
harshpreet284 <harshpreetsingh2643@gmail.com>
2025-08-04 11:32:45 +0530

    add

---
+++ Javascript/Lect 42/index.js
@@ -0,0 +1,39 @@
+// for(let i=1;i<=10;i++){
+//     console.log("Harsh");
+// }
+
+// for(let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+// for(let i=5;i>0;i--){
+//     console.log(i);
+// }
+
+// for(let i=1;i<=6;i++){
+//     if(i==4){
+//         break;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// for(let i=1;i<=4;i++){
+//     if(i==3){
+//         continue;
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// let firstName= "Harshpreet";
+// let lastName = "Singh";
+// let name = `This 
+// is my 
+// Name`
+// console.log(name);
+
+let companyName = new String ("Happy Buy");
+console.log(companyName);
