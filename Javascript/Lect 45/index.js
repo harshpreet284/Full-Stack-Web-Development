f43df14b7149274f047637bd2ad2dca465b5a191
harshpreet284 <harshpreetsingh2643@gmail.com>
2025-08-05 14:24:08 +0530

    add

---
+++ Javascript/Lect 45/index.js
@@ -0,0 +1,95 @@
+{
+    // Function Hoisting
+
+    // sayMyName("Harshpreet Singh");
+
+    // function sayMyName(name){
+    //     console.log(name);
+    // }
+
+    // Variable Hoisting
+
+    // console.log(age);
+    // var age = 24;
+
+    // Reference Errors
+
+    // console.log(age);
+    // let age = 24;
+
+    // console.log(age);
+    // const age = 23;
+
+    
+    // sayName();
+    // let sayName = function() {
+    //     console.log ("Harshpreet Singh");
+    // }
+
+    // sayName();
+    // let sayName = () =>{
+    //     console.log ("Harshpreet Singh");
+    // }
+
+    // const obj = new Student();
+    // class Student{
+
+    // }
+
+    // Functions are First Class Citizens
+
+    // let greet = function(){
+    //     console.log("Greeting of the day");
+    // }
+
+    // greet();
+
+
+    // function greetme(greet , name){
+    //     console.log("Helloi" , name);
+    //     greet();
+    // }
+
+    // function greet(){
+    //     console.log("greetings of the day");
+    // }
+
+    // greetme(greet , "Harsh");
+
+    // function solve(number){
+    //     return function(number){
+    //         return number*number;
+    //     }
+    // }
+    // let ans= solve(5);
+    // let finalans= ans(10);
+    // console.log(finalans);
+
+    // const arr=[
+    //     function (a,b){
+    //         return a+b;
+    //     },
+    //     function (a,b){
+    //         return a-b;
+    //     },
+    //     function (a,b){
+    //         return a*b;
+    //     }
+    // ];
+    // let first= arr[2];
+    // let ans= first(5,10);
+    // console.log(ans);
+
+    // let obj={
+    //     age: 23,
+    //     wt: 83,
+    //     greet:()=>{
+    //         console.log("Hello");
+    //     }
+    // }
+    // console.log(obj.age);
+    // obj.greet();
+
+    
+
+}
