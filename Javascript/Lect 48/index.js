b33ab203bcb85293c5c2b5e526bef8dac8e20648
harshpreet284 <harshpreetsingh2643@gmail.com>
2025-08-06 15:13:01 +0530

    add

---
+++ Javascript/Lect 48/index.js
@@ -0,0 +1,31 @@
+// Built In Object = Math
+
+// console.log(Math.PI);
+
+// console.log(Math.max(123,42,56,346,223,5666));
+
+// console.log(Math.min(2,53,52,45,234,135,6646));
+
+// console.log(Math.round(2.6));
+
+// console.log(Math.floor(2.9));
+
+// console.log(Math.ceil(2.1));
+
+// console.log(Math.abs(-6));
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(5));
+
+// console.log(Math.pow(2,10));
+
+
+
+// Built In Object = Date
+
+let curr = new Date();
+let date = new Date("July 20 2011 06:49");
+let newDate = new Date(2011,6,20,7);
+console.log(newDate.setFullYear(2001));
+console.log(newDate);
